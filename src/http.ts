c238e4e00b2d797571ad77b55a44b5dc0cb4ac3a
dhirajmangal <dhirajmangal@gmail.com>
2026-06-29 22:18:07 +0400

    Load custom Pipedrive MCP tool wrapper

--- src/http.ts
+++ src/http.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import { randomUUID } from "crypto";
 import { StreamableHTTPServerTransport } from "@modelcontextprotocol/sdk/server/streamableHttp.js";
-import { createServer } from "./index.js";
+import { createServer } from "./index-custom.js";
 
 const app = express();
 app.use(express.json());
